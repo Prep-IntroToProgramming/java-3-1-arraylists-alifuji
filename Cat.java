e0a8b1d3790f697bbbcc56bac8be0b4bb7b0b040
Alison Fujimoto <19fujimotoa@flintridgeprep.org>
2018-11-20 00:21:56 -0800

    added comments to both classes

--- Cat.java
+++ Cat.java
@@ -1,29 +1,35 @@
 
 public class Cat {
+    //declaring name & weight variables, making them private
     private String name;
     private double weight;
+    //getter/setter methods for name and weight variables
     public void setName(String newName) {
+        //assigning private variable name to the new name
         name = newName;
     }
 
     public void setWeight(double newWeight) {
+        //assigning private variable weight to the new weight
         weight = newWeight;
     }
 
     public String getName() {
+        //returning the private variable name
         return name;
     }
 
     public double getWeight() {
+        //returning the private variable weight
         return weight;
     }
-
+    //constructor w/out parameters
     public Cat () {
 
     }
-
+    //constructor that takes a String and assigns the name instance variable to it
     public Cat(String name) {
-
+        
     }
-    System.out.println("Meow!");
+    
 }
--- TestCatList.java
+++ TestCatList.java
@@ -3,10 +3,15 @@ import java.util.*;
 public class TestCatList
 {
     public static void main(String[] args) {
+        //size of ArrayList (4 cats)
         int n = 4;
+        //creating a new ArrayList of 4 cats called catList
         ArrayList<Cat> catList = new ArrayList<Cat>(4);
+        //enhanced for loop for creating new cat elements and assigning names to each 
         for (int i = 0; i <= n; i++) {
+            //creating new cat element, adding it into the catList
             catList.add(new Cat());
+            //using a getter method to get element 0 of the catList and assign it a name
             catList.get(0).setName("Charlie");
 
             catList.add(new Cat());
@@ -19,11 +24,15 @@ public class TestCatList
             catList.get(3).setName("Winnie");
 
         }
-        for (int i = 0; 5<i<15; i++) {
-             Math.random()*10=5;
+        //enhanced for loop to generate a random weight between 5 & 15 for each cat
+        for (int i = 0; i<5; i++) {
+            double randomWeight = Math.random()*10+5;
+            catList.get(i).setWeight(randomWeight);
         }
-        for (int i = 0; i<catList.size(); i_++){
-            System.out.print("The cat's name and weight are" +catList.get(i);
+        //enhanced for loop to print out the name & weight of each cat; each cat says meow after name/weight is printed
+        for (int i = 0; i<catList.size(); i++){
+            System.out.println("The cat's name and weight are" +catList.get(i));
+            System.out.println("Meowwww!");
         }
     }
 }
