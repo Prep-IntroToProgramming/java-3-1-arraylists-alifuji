b23ab6ef79b537a71790b92eac6384f0a9337080
Alison Fujimoto <19fujimotoa@flintridgeprep.org>
2018-11-19 23:16:49 -0800

    added body to Cat and TestCatList classes

--- Cat.java
+++ Cat.java
@@ -1,3 +1,29 @@
 
 public class Cat {
+    private String name;
+    private double weight;
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public void setWeight(double newWeight) {
+        weight = newWeight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public Cat () {
+
+    }
+
+    public Cat(String name) {
+
+    }
+    System.out.println("Meow!");
 }
---
+++ TestCatList.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class TestCatList
+{
+    ArrayList<Cat> catList = new ArrayList<Cat> ();
+    catList.add(new Cat());
+    catList.get(0).setName("Charlie");
+    
+}
